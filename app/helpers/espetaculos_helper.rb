35ff67fc30b414a5ef037168f03359e144e329b6
Gabriel Fernandes Monteiro <gabriel.monteiro@intuiti.com.br>
2016-08-01 13:39:28 -0300

    #199 correção no tratamento de parâmetros get

--- app/helpers/espetaculos_helper.rb
+++ app/helpers/espetaculos_helper.rb
@@ -3,7 +3,7 @@ module EspetaculosHelper
 	def preserv_params
 		@params = ""
 	  	request.query_parameters.each do |key, value|
-	      @params += "&" + sanitize(key)  + "=" + sanitize(value)
+	      @params += "&" + u(key)  + "=" + u(value)
 	  	end
 	  	@params
 	end
