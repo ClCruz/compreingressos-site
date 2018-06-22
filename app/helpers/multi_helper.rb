e646bf54d386f7a674b212f3279c77baae903e77
Bruno Luigi Coccaro <blcoccaro@gmail.com>
2018-06-22 12:37:14 -0300

    Alterações para as cores da ingresso litoral e para ser multi site

--- app/helpers/multi_helper.rb
+++ app/helpers/multi_helper.rb
@@ -1,8 +1,15 @@
 module MultiHelper
+    module_function
+
     def SizeOfMiniature
         #'160' #compreingressos
 
         '320' #ingressoslitoral
     end
-    module_function :SizeOfMiniature
+    def getLogo
+        #'../images/menu_logo.png' #compreingressos
+
+        '../images/multi/logo_header.png' #ingressoslitoral
+    end
+    #module_function :SizeOfMiniature :getLogo
 end
