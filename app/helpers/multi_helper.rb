8ff36488ab1369434603dd27a5700c74f987cc21
Bruno Luigi Coccaro <blcoccaro@gmail.com>
2018-07-02 14:04:22 -0300

    Alterações para as cores da ingresso litoral e para ser multi site

--- app/helpers/multi_helper.rb
+++ app/helpers/multi_helper.rb
@@ -9,7 +9,61 @@ module MultiHelper
     def getLogo
         #'../images/menu_logo.png' #compreingressos
 
-        '../images/multi/logo_header.png' #ingressoslitoral
+        '../images/multi_litoralingressos/logo_header.png' #ingressoslitoral
     end
+    def getFavico
+        #'/favicon.ico' #compreingressos
+
+        '/images/multi_litoralingressos/favicon.ico' #ingressoslitoral
+    end
+    def getTitulo
+        #'COMPREINGRESSOS.COM - Gestão de bilheteria e venda de ingressos para teatros e casas de show' #compreingressos
+
+        'INGRESSOSLITORAL.COM - Venda de ingressos para teatros e casas de show' #ingressoslitoral
+    end
+    def getName
+        #'COMPREINGRESSOS.COM' #compreingressos
+
+        'INGRESSOSLITORAL.COM' #ingressoslitoral
+    end
+    def getFacebook
+        #'http://www.facebook.com/compreingressos' #compreingressos
+
+        '' #ingressoslitoral
+    end
+    def getTwitter
+        #'http://twitter.com/compreingressos' #compreingressos
+
+        '' #ingressoslitoral
+    end
+    def getBlog
+        #'http://blog.compreingressos.com/' #compreingressos
+
+        '' #ingressoslitoral
+    end
+    def getInstagram
+        #'https://www.instagram.com/compreingressos' #compreingressos
+
+        '' #ingressoslitoral
+    end
+    def getYoutube
+        #'https://www.youtube.com/compreingressos' #compreingressos
+
+        '' #ingressoslitoral
+    end
+    def getGooglePlus
+        #'https://plus.google.com/b/107039038797259256027/107039038797259256027' #compreingressos
+
+        '' #ingressoslitoral
+    end
+    def getTomTicket
+        #'https://compreingressos.tomticket.com/kb/' #compreingressos
+
+        '' #ingressoslitoral
+    end
+    def getHasMidiaSocial
+        getFacebook != '' && getTwitter != ''
+    end
+    
     #module_function :SizeOfMiniature :getLogo
 end
