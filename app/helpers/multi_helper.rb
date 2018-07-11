fc3433b9d4f863cca6664d7cd4cab065ffce937a
Bruno Luigi Coccaro <blcoccaro@gmail.com>
2018-07-11 12:13:11 -0300

    Correções referente a multi-site

--- app/helpers/multi_helper.rb
+++ app/helpers/multi_helper.rb
@@ -1,72 +1,184 @@
 module MultiHelper
     module_function
+    #@@site = "compreingressos"
+    @@site = "ingressoslitoral"
 
+    def getSite
+        @@site
+    end
     def SizeOfMiniature
-        #'160' #compreingressos
-
-        '320' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            '160'
+        when 'ingressoslitoral'
+            '320'
+        end
     end
     def getLogo
-        #'../images/menu_logo.png' #compreingressos
-
-        '../images/multi_litoralingressos/logo_header.png' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            '../images/menu_logo.png'
+        when 'ingressoslitoral'
+            '../images/multi_litoralingressos/logo_header.png'
+        end
     end
     def getFavico
-        #'/favicon.ico' #compreingressos
-
-        '/images/multi_litoralingressos/favicon.ico' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            '/favicon.ico'
+        when 'ingressoslitoral'
+            '/images/multi_litoralingressos/favicon.ico'
+        end
     end
     def getTitulo
-        #'COMPREINGRESSOS.COM - Gestão de bilheteria e venda de ingressos para teatros e casas de show' #compreingressos
-
-        'INGRESSOSLITORAL.COM - Venda de ingressos para teatros e casas de show' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'COMPREINGRESSOS.COM - Gestão de bilheteria e venda de ingressos para teatros e casas de show'
+        when 'ingressoslitoral'
+            'INGRESSOSLITORAL.COM - Venda de ingressos para teatros e casas de show'
+        end
     end
     def getName
-        #'COMPREINGRESSOS.COM' #compreingressos
-
-        'INGRESSOSLITORAL.COM' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'COMPREINGRESSOS.COM'
+        when 'ingressoslitoral'
+            'INGRESSOSLITORAL.COM'
+        end
     end
     def getFacebook
-        #'http://www.facebook.com/compreingressos' #compreingressos
-
-        '' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'http://www.facebook.com/compreingressos'
+        when 'ingressoslitoral'
+            ''
+        end
     end
     def getTwitter
-        #'http://twitter.com/compreingressos' #compreingressos
-
-        '' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'http://twitter.com/compreingressos'
+        when 'ingressoslitoral'
+            ''
+        end
     end
     def getBlog
-        #'http://blog.compreingressos.com/' #compreingressos
-
-        '' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'http://blog.compreingressos.com/'
+        when 'ingressoslitoral'
+            ''
+        end
     end
     def getInstagram
-        #'https://www.instagram.com/compreingressos' #compreingressos
-
-        '' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'https://www.instagram.com/compreingressos'
+        when 'ingressoslitoral'
+            ''
+        end
     end
     def getYoutube
-        #'https://www.youtube.com/compreingressos' #compreingressos
-
-        '' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'https://www.youtube.com/compreingressos'
+        when 'ingressoslitoral'
+            ''
+        end
     end
     def getGooglePlus
-        #'https://plus.google.com/b/107039038797259256027/107039038797259256027' #compreingressos
-
-        '' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'https://plus.google.com/b/107039038797259256027/107039038797259256027'
+        when 'ingressoslitoral'
+            ''
+        end
     end
     def getTomTicket
-        #'https://compreingressos.tomticket.com/kb/' #compreingressos
-
-        '' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'https://compreingressos.tomticket.com/kb/'
+        when 'ingressoslitoral'
+            ''
+        end
     end
     def getHasMidiaSocial
         getFacebook != '' && getTwitter != ''
     end
     def getURICadastro
-        #'https://compra.compreingressos.com/comprar/minha_conta.php' #compreingressos
-        'https://compra.ingressoslitoral.com/comprar/minha_conta.php' #ingressoslitoral
+        case @@site
+        when 'compreingressos'
+            'https://compra.compreingressos.com/comprar/minha_conta.php'
+        when 'ingressoslitoral'
+            'https://compra.ingressoslitoral.com/comprar/minha_conta.php'
+        end
+    end
+    def borderoWebURI
+        case @@site
+        when 'compreingressos'
+            'https://compra.compreingressos.com/comprar/loginBordero.php?redirect=..%2Fadmin%2F%3Fp%3DrelatorioBordero'
+        when 'ingressoslitoral'
+            'https://compra.ingressoslitoral.com/comprar/loginBordero.php?redirect=..%2Fadmin%2F%3Fp%3DrelatorioBordero'
+        end
+    end
+    def meiaEntradaURI
+        case @@site
+        when 'compreingressos'
+            '/meia_entrada'
+        when 'ingressoslitoral'
+            '/meia-entrada-por-regiao'
+        end
+    end
+
+    def footerMenu (nome)
+        case @@site
+        when 'compreingressos'
+            true
+        when 'ingressoslitoral'
+            case nome
+            when 'SERVIÇOS'
+                true
+            when 'Captação de patrocínio'
+                false
+            when 'Catracas online e offline'
+                false
+            when 'Central de vendas'
+                false
+            when 'Credenciamento'
+                true
+            when 'Gestão de bilheteria'
+                true
+            when 'Ingressos'
+                true
+            when 'Vendas para grupos'
+                false
+            when 'Vendas pela internet'
+                true
+            when 'Vantagens do sistema'
+                true
+            when 'AJUDA'
+                true
+            when 'Borderô web'
+                true
+            when 'Institucional'
+                false
+            when 'Lei 6103/11'
+                true
+            when 'Perguntas frequentes'
+                false
+            when 'Política de venda'
+                false
+            when 'Privacidade'
+                false
+            when 'Política de Meia Entrada'
+                true
+            when 'Pontos de Venda'
+                false
+            when 'Política de Cancelamento'
+                true
+            end
+        end
+
     end
     
     #module_function :SizeOfMiniature :getLogo
